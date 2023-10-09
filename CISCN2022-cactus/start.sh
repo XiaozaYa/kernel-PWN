6293e77020650a059f5fedc4b040d06b57eaa590
xiaozaya <2353567071@qq.com>
2023-10-09 18:36:38 +0800

    msg_msg+pipe_buffer

---
+++ CISCN2022-cactus/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+qemu-system-x86_64 \
+    -m 128M \
+    -kernel ./bzImage \
+    -initrd ./rootfs.cpio \
+    -monitor /dev/null \
+    -append "root=/dev/ram console=ttyS0 oops=panic quiet panic=1 kaslr" \
+    -cpu kvm64,+smep,+smap\
+    -netdev user,id=t0, -device e1000,netdev=t0,id=nic0 \
+    -nographic \
+    -no-reboot \
+    -s
